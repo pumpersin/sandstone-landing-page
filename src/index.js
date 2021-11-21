087b3c36944e4f21dfd7dcf20709bfc5ecfa64db
Param Siddharth <contact@paramsid.com>
2021-11-21 20:02:48 +0530

    Create header

--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
 import 'bootstrap';
+import $ from 'jquery';
 
 import '@fontsource/montserrat';
 import '/scss/style.scss';
+
+$(() => {
+	$('button.hamburger').on('click', async function() {
+		$(this).toggleClass('is-active');
+		const collapsibleID = $(this).attr('data-bs-target');
+		const collapsible = $(collapsibleID);
+		while (collapsible.hasClass('collapsing')) await new Promise(r => setTimeout(r, 500));
+		const shown = collapsible.hasClass('show');
+		if (shown)
+			$(this).addClass('is-active');
+		else
+			$(this).removeClass('is-active');
+	});
+});
